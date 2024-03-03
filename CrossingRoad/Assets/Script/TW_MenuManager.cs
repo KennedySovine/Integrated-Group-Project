6880edce7065e926b4bbf8492d505975877f4bb0
Tegan Waterer <t.waterer1@uni.brighton.ac.uk>
2024-03-03 17:08:22 +0000

    Level UI updates

--- CrossingRoad/Assets/Script/TW_MenuManager.cs
+++ CrossingRoad/Assets/Script/TW_MenuManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TW_MenuManager : MonoBehaviour
 {
+
+    public void PlayLevel()
+    {
+        SceneManager.LoadSceneAsync("LevelScene");
+    }
+
+
 
     public void QuitGame()
     {
