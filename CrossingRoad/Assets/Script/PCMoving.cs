60967cddb956c29cd8c765769c24aead54470f6a
KennedySovine <89476714+KennedySovine@users.noreply.github.com>
2024-04-29 17:15:25 +0100

    All Assets Imported
    All assets are imported. Level template almost complete. Need to add some default shit for the UI and text pop ups. Still need to add planes for the traffic lights to show properly.
    
    Co-Authored-By: Joseph Carroll <150065565+JCarroll15@users.noreply.github.com>

--- CrossingRoad/Assets/Script/PCMoving.cs
+++ CrossingRoad/Assets/Script/PCMoving.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PCMoving : MonoBehaviour
 {
+
+    public float speed = 2;
+
+    public GameObject gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameManager.GetComponent<gameManager>().requirementsMet())
+        {
+            if (gameManager.GetComponent<gameManager>().spacePressed)
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+        }
     }
 }
---
+++ CrossingRoad/Assets/Script/gameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+
+    public bool lookedLeft = false;
+    public bool lookedRight = false;
+
+    public bool mustCheck = true;
+
+    public bool spacePressed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("space key was pressed");
+            if (!requirementsMet())
+            {
+                //Pop up a message bar that tells the player to make sure to check left and right
+            }
+            else
+            {
+                spacePressed = true;
+            }
+        }
+    }
+
+    public bool requirementsMet()
+    {
+        if (!mustCheck)
+        {
+            return true;
+        }
+        if (lookedLeft && lookedRight)
+        {
+            return true;
+        }
+        return false;
+    }
+}
